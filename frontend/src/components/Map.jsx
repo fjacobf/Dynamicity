c50ac9f2a754a0144851cac50c76842421d8ac87
fojacob <up202102359@up.pt>
2024-04-10 15:01:07 +0100

    Added the us polygon example

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react'
-import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet'
+import { MapContainer, Marker, Polygon, Popup, TileLayer, useMap } from 'react-leaflet'
+import { statesData } from '../assets/us-states'
 
 function ResetCenterView(props) {
   const { selectPosition } = props
@@ -30,6 +31,51 @@ function Map(props) {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      {
+        statesData.features.map((state) => {
+          const coordinates = state.geometry.coordinates[0].map(item => [item[1], item[0]])
+
+          return (
+            // eslint-disable-next-line react/jsx-key
+            <Polygon
+              pathOptions={{
+                fillColor: '#FD8D3C',
+                fillOpacity: 0.7,
+                weight: 2,
+                opacity: 1,
+                dashArray: 3,
+                color: 'white',
+              }}
+              positions={coordinates}
+              eventHandlers={{
+                mouseover: (e) => {
+                  const layer = e.target
+                  layer.setStyle({
+                    fillOpacity: 1,
+                    weight: 5,
+                    dashArray: '',
+                    color: '#666',
+                    fillColor: '#D45962',
+                  })
+                },
+                mouseout: (e) => {
+                  const layer = e.target
+                  layer.setStyle({
+                    fillOpacity: 0.7,
+                    weight: 2,
+                    dashArray: '3',
+                    color: 'white',
+                    fillColor: '#FD8D3C',
+                  })
+                },
+                click: () => {
+                  console.log(state.properties.name)
+                },
+              }}
+            />
+          )
+        })
+      }
       {selectPosition && (
         <Marker position={locationSelection}>
           <Popup>
