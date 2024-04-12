c655a4f93872f4593d96496650cc203f3b58658d
fojacob <up202102359@up.pt>
2024-04-12 17:48:49 +0100

    created classes file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import './App.css'
 import Map from './components/Map'
 import SearchBar from './components/SearchBar'
 
---
+++ frontend/src/classes.js
@@ -0,0 +1,5 @@
+export class Test {
+  hello() {
+    console.log('hello')
+  }
+}
